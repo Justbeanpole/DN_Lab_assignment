6bf1e105c4a50e45b6101db146bfa206227e71b3
justbeanpole <dbtjd7282@gmail.com>
2025-07-18 17:12:55 +0900

    refactor : 의미있는 변수로 수정

--- DN_Assignment/4096.c
+++ DN_Assignment/4096.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main_4096(void) {
-    for (int l = 0; l < 8; l++) {
-        for (int k = 0; k < 8; k++) {
-            for (int i = 0; i < 8; i++) {
-                for (int j = 1; j <= 8; j++) {
-                    printf("%5d", j + (k * 8) + (i * 8*8) + (l * 8*8*8));
+    for (int line = 0; line < 8; line++) {
+        for (int col = 0; col < 8; col++) {
+            for (int block = 0; block < 8; block++) {
+                for (int row = 1; row <= 8; row++) {
+                    printf("%5d", row + (col * 8) + (block * 8*8) + (line * 8*8*8));
                 }
                 printf(" |");
             }
